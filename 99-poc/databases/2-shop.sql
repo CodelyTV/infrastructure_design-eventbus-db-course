0021f9f9e2a79136eadba2316bf63654ab2bdb93
Rafa Gómez <rgomezcasas@gmail.com>
2025-11-10 11:11:51 +0100

    feat(poc): add new use cases

--- 99-poc/databases/2-shop.sql
+++ 99-poc/databases/2-shop.sql
@@ -1,18 +1,24 @@
-/* -------------------------
-        SHOP CONTEXT
----------------------------- */
+CREATE TABLE shop.products (
+	id UUID PRIMARY KEY,
+	name VARCHAR(255),
+	price_amount DECIMAL(10, 2),
+	price_currency CHAR(3),
+	image_urls JSON,
+	views INT,
+	creation_date TIMESTAMP
+);
 
-CREATE TABLE shop__users (
+CREATE TABLE shop.users (
 	id UUID PRIMARY KEY,
 	name VARCHAR(255),
 	email VARCHAR(255),
 	profile_picture VARCHAR(255)
 );
 
-CREATE TABLE shop__product_reviews (
+CREATE TABLE shop.product_reviews (
 	id UUID PRIMARY KEY,
-	user_id UUID REFERENCES shop__users(id),
-	product_id UUID REFERENCES seller_backoffice__products(id),
+	user_id UUID,
+	product_id UUID,
 	rating FLOAT,
 	comment VARCHAR(500),
 	is_featured BOOLEAN DEFAULT FALSE
