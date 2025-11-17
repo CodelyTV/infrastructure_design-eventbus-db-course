f3b9a6235290db443d3f135d878ce17695324c0c
Rafa Gómez <rgomezcasas@gmail.com>
2025-11-17 12:22:02 +0100

    feat(02-2): improve example

--- 02-publish_messages/2-retry/src/contexts/shared/infrastructure/domain-event/PostgresEventBus.ts
+++ 02-publish_messages/2-retry/src/contexts/shared/infrastructure/domain-event/PostgresEventBus.ts
@@ -3,6 +3,7 @@ import { JSONValue, Row, TransactionSql } from "postgres";
 
 import { DomainEvent } from "../../domain/event/DomainEvent";
 import { EventBus } from "../../domain/event/EventBus";
+import { retry } from "../../domain/retry";
 import { PostgresConnection } from "../postgres/PostgresConnection";
 
 @Service()
@@ -10,14 +11,14 @@ export class PostgresEventBus implements EventBus {
 	constructor(private readonly connection: PostgresConnection) {}
 
 	async publish(events: DomainEvent[]): Promise<void> {
-		await this.retry(async () => await this.publishEvents(events));
-	}
-
-	async publishEvents(events: DomainEvent[]): Promise<void> {
 		if (events.length === 0) {
 			return;
 		}
 
+		await retry(async () => await this.publishEvents(events), 3, 30);
+	}
+
+	async publishEvents(events: DomainEvent[]): Promise<void> {
 		await this.connection.sql.begin(async (tx) => {
 			await Promise.all(
 				events.map((event) => this.insertEvent(event, tx)),
@@ -39,32 +40,5 @@ export class PostgresEventBus implements EventBus {
 				${event.occurredOn}
 			)
 		`;
-	}
-
-	private async retry<T>(fn: () => Promise<T>): Promise<T> {
-		return this.retryWithAttempt(fn, 1, 3);
-	}
-
-	private async retryWithAttempt<T>(
-		fn: () => Promise<T>,
-		attempt: number,
-		maxAttempts: number,
-	): Promise<T> {
-		try {
-			return await fn();
-		} catch (error) {
-			if (attempt >= maxAttempts) {
-				throw error;
-			}
-			await this.wait(30 * attempt);
-
-			return this.retryWithAttempt(fn, attempt + 1, maxAttempts);
-		}
-	}
-
-	private async wait(ms: number): Promise<void> {
-		return new Promise((resolve) => {
-			setTimeout(resolve, ms);
-		});
 	}
 }
